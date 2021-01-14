4348368f47fc74b6605c256e84375e599c701304
Moein Aghamirzaei <moein.6556@gmail.com>
2021-01-14 21:06:23 +0330

    init

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # linux-font-installer
-install many font in one step
+install many fonts in one step
+
+### installation
+```
+git clone https://github.com/mrmoein/linux-font-installer
+cd ~
+nano .bashrc
+```
+add this to end of file
+```
+alias font-installer="python3 /path/to/directory/install-fonts/font-installer.py"
+```
+**note**: replace `/path/to/directory` with current application path
+
+now you can run `font-installer` commend everywhere
---
+++ font-installer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+import glob, os
+from shutil import copyfile
+from pathlib import Path
+
+types = ["*.ttf", '*.TTF', "*.otf", "*.OTF"]
+fonts_dir = '{}/.local/share/fonts/font-installer-by-moein'.format(Path.home())
+
+if not os.path.exists(fonts_dir):
+    os.mkdir(fonts_dir)
+    print('font-installer-by-moein directory is created')
+
+os.chdir(os.getcwd())
+for type in types:
+    for file in glob.glob(type):
+        if not os.path.exists(fonts_dir + file):
+            copyfile(file, fonts_dir + file)
+            print("{}: installed".format(file))
+        else:
+            print("{}: already exist".format(file))
+print("Done")
+os.system('fc-cache -f -v')
