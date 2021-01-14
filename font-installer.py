a4f4bba17fe2945b2db4d7a4a6c0d809108930e4
Moein Aghamirzaei <moein.6556@gmail.com>
2021-01-14 21:52:45 +0330

    add sleep after copying all fonts

--- font-installer.py
+++ font-installer.py
@@ -2,9 +2,10 @@
 import glob, os
 from shutil import copyfile
 from pathlib import Path
+import time
 
 types = ["*.ttf", '*.TTF', "*.otf", "*.OTF"]
-fonts_dir = '{}/.local/share/fonts/font-installer-by-moein'.format(Path.home())
+fonts_dir = '{}/.local/share/fonts/font-installer-by-moein/'.format(Path.home())
 
 if not os.path.exists(fonts_dir):
     os.mkdir(fonts_dir)
@@ -19,4 +20,6 @@ for type in types:
         else:
             print("{}: already exist".format(file))
 print("Done")
+time.sleep(2)
+
 os.system('fc-cache -f -v')
