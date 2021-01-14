6bc0dfb0e8c32e8b4ab29b6601a0d0124a2e0658
Moein Aghamirzaei <moein.6556@gmail.com>
2021-01-14 22:13:14 +0330

    fix subdir fonts finder

--- font-installer.py
+++ font-installer.py
@@ -1,25 +1,22 @@
 #!/usr/bin/python
-import glob, os
+import os
 from shutil import copyfile
 from pathlib import Path
-import time
 
-types = ["*.ttf", '*.TTF', "*.otf", "*.OTF"]
-fonts_dir = '{}/.local/share/fonts/font-installer-by-moein/'.format(Path.home())
+types = [".ttf", '.TTF', ".otf", ".OTF"]
+all_fonts_dirpath = '{}/.local/share/fonts/font-installer-by-moein/'.format(Path.home())
 
-if not os.path.exists(fonts_dir):
-    os.mkdir(fonts_dir)
-    print('font-installer-by-moein directory is created')
+for font_type in types:
+    for dirpath, dirnames, filenames in os.walk("."):
+        for filename in [f for f in filenames if f.endswith(font_type)]:
+            # print(os.path.join(dirpath, filename))
+            font_path = dirpath + '/' + filename
+            if not os.path.exists(all_fonts_dirpath + '/' + filename):
+                copyfile(font_path,  all_fonts_dirpath + "/" + filename)
+                print("{}: installed".format(font_path))
+            else:
+                print("{}: already exist".format(font_path))
 
-os.chdir(os.getcwd())
-for type in types:
-    for file in glob.glob(type):
-        if not os.path.exists(fonts_dir + file):
-            copyfile(file, fonts_dir + file)
-            print("{}: installed".format(file))
-        else:
-            print("{}: already exist".format(file))
-print("Done")
-time.sleep(2)
+print("Done!")
 
 os.system('fc-cache -f -v')
