26d8af21bd1badaf93bd372a8182e504a14622ad
Moein Aghamirzaei <moein.6556@gmail.com>
2021-03-05 00:26:49 +0330

    create fonts directory if not exist and add some comment

--- font-installer.py
+++ font-installer.py
@@ -4,28 +4,34 @@ from shutil import copyfile
 from pathlib import Path
 from bcolors import bcolors
 
+# all fonts type
 types = [".ttf", '.TTF', ".otf", ".OTF"]
-all_fonts_dirpath = '{}/.local/share/fonts/font-installer-by-moein/'.format(Path.home())
+all_fonts_dir_path = '{}/.local/share/fonts/font-installer-by-moein/'.format(Path.home())
+
+# create fonts directory if not exist
+if not os.path.exists(directory):
+    Path(all_fonts_dir_path).mkdir(parents=True, exist_ok=True)
 
 
-def printC(text, style, newLine=False):
+# print colored text
+def print_c(text, style, newLine=False):
     if newLine:
         print(style + text + bcolors.ENDC, end='')
     else:
         print(style + text + bcolors.ENDC)
 
 
+# copy all font to all_fonts_dir_path
 for font_type in types:
     for dirpath, dirnames, filenames in os.walk("."):
         for filename in [f for f in filenames if f.endswith(font_type)]:
-            # print(os.path.join(dirpath, filename))
             font_path = dirpath + '/' + filename
-            if not os.path.exists(all_fonts_dirpath + '/' + filename):
-                copyfile(font_path,  all_fonts_dirpath + "/" + filename)
-                printC("{}: installed".format(font_path), bcolors.OKGREEN)
+            if not os.path.exists(all_fonts_dir_path + '/' + filename):
+                copyfile(font_path, all_fonts_dir_path + "/" + filename)
+                print_c("{}: installed".format(font_path), bcolors.OKGREEN)
             else:
-                printC("{}: already exist".format(font_path), bcolors.WARNING)
+                print_c("{}: already exist".format(font_path), bcolors.WARNING)
 
-printC("============== Done! ==============", bcolors.HEADER)
+print_c("============== Done! ==============", bcolors.HEADER)
 
 os.system('fc-cache -f -v')
